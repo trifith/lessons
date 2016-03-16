63abd7156fc6f62683a283644bc07317589bcb19
Sean Murphy <trifith@gmail.com>
2016-03-16 12:04:14 -0400

    Exercise 5 complete

--- object/DataOnly.java
+++ object/DataOnly.java
@@ -11,5 +11,12 @@ class DataOnly {
 		System.out.println(data.i);
 		System.out.println(data.d);
 		System.out.println(data.b);
+		System.out.println("Changing Variables for Ex 5");
+		data.i = 999;
+		data.d = 3.141592;
+		data.b = true;
+		System.out.println(data.i);
+		System.out.println(data.d);
+		System.out.println(data.b);
 	}
 }
