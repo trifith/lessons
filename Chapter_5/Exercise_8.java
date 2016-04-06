d0e260bb1ff6443a7e7aba38f714d712b35e39fa
Sean Murphy <trifith@gmail.com>
2016-04-06 14:11:17 -0400

    Exercise 8 Complete

--- Chapter_5/Exercise_8.java
+++ Chapter_5/Exercise_8.java
@@ -1,11 +1,11 @@
 public class Exercise_8 {
 
-	public void method_1(Object myobj) {
+	public void method_1(Exercise_8 myobj) {
 		System.out.println(myobj);
 	}
-	public void method_2(Object ex1) {
-		this.method_1(ex1);
-		ex1.method_1("string2");
+	public void method_2(Exercise_8 ex2) {
+		this.method_1(ex2);
+		ex2.method_1(ex2);
 	}
 	public static void main(String[] args) {
 		Exercise_8 ex1 = new Exercise_8();
