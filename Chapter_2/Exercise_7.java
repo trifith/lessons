7defa7465dc1e36f3ccf2713e1e377cf76807694
Sean Murphy <trifith@gmail.com>
2016-04-08 13:52:19 -0400

    Exercise 7 complete

---
+++ Chapter_2/Exercise_7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Exercise_7 {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int i = rand.nextInt(100);
+		System.out.println(i > 50);
+	}
+}
