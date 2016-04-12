84bd9046ef0b55c510da53e2a3c63ed1e97a06f3
Sean Murphy <trifith@gmail.com>
2016-04-12 12:42:39 -0400

    Exercise 9 Complete

---
+++ Chapter_2/Exercise_9.java
@@ -0,0 +1,18 @@
+public class Exercise_9 {
+
+	public static void main(String[] args) {
+		Double bigDouble = 1.7976931348623157e308;
+		Float bigFloat = 3.4028235e38f;
+		Float littleFloat = 1.4e-45f;
+		Double littleDouble = 4.9e-324;
+		System.out.println("Big Double:    " + bigDouble);
+		System.out.println("Big Float:     " + bigFloat);
+		System.out.println("Little Double: " + littleDouble);
+		System.out.println("Little Float:  " + littleFloat);
+	}
+}
+
+//3.4028235E38
+//1.7976931348623157E308
+//1.4E-45
+//4.9E-324
